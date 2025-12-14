17c81f77b174155546a696df1903d53a858e2449
Godwin Boakye <ohenebayb@gmail.com>
2025-12-14 16:54:07 +0000

    task_5 review changes

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,3 @@
 USE alx_book_store;
-INSERT into customer (
-    customer_id, customer_name, email, address
-)
-VALUES(
-    1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave."
-);
+INSERT into customer(customer_id, customer_name, email, address)
+VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
